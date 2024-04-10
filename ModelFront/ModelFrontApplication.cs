150318f3738d4be217d23b88c7fe9bcec685af39
bZverok <137277669+bZverok@users.noreply.github.com>
2024-04-10 13:08:35 +0300

    added categories

--- ModelFront/ModelFrontApplication.cs
+++ ModelFront/ModelFrontApplication.cs
@@ -1,9 +1,16 @@
 ﻿using Blackbird.Applications.Sdk.Common;
+using Blackbird.Applications.Sdk.Common.Metadata;
 
 namespace Apps.ModelFront;
 
-public class ModelFrontApplication : IApplication
+public class ModelFrontApplication : IApplication, ICategoryProvider
 {
+    public IEnumerable<ApplicationCategory> Categories
+    {
+        get => [ApplicationCategory.MachineTranslationAndMtqe];
+        set { }
+    }
+    
     public string Name
     {
         get => "ModelFront";
